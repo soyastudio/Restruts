4ed1b90105c4634c7dba902bd7b9254e00e37d84
Qun Wen <wenqun.soya@gmail.com>
2022-12-02 10:03:43 -0800

    Fragment and Proxy

--- restruts/src/main/java/soya/framework/action/Resource.java
+++ restruts/src/main/java/soya/framework/action/Resource.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.nio.charset.Charset;
 
 public interface Resource {
+
     String getAsString(Charset encoding) throws ResourceException;
 
     InputStream getAsInputStream() throws ResourceException;
